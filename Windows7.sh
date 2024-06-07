eef08494891f6d1685a3b78367e3344c1e84d3f5
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-06-07 18:10:19 +0300

    Update Windows7.sh

--- Windows7.sh
+++ Windows7.sh
@@ -1,41 +1,33 @@
 #!/bin/bash
-#Forked from @JCGAMESCLASSICOS
-# Excluir todos  os containers a primeira linha 
-
-
-
+# Forked from @JCGAMESCLASSICOS
+# Delete all containers in the first line
 
 #docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
-#Instalando o Docker e o Firefox Nyghtly
+# Installing Docker and Firefox Nightly
 curl -L docker.batocera.pro | bash
 curl -L ffnightly.batocera.pro | bash
 
-
-
-# Criar diretório para os arquivos que vão ser chamados
+# Create directory for the files to be called
 mkdir -p /userdata/roms/microsoft
 mkdir -p /userdata/roms/microsoft/windows7
 
-# Definir ID do arquivo
+# Set file ID
 curl -L -o /userdata/roms/microsoft/windows7/docker-compose.yml "https://github.com/garbagescow/Windows/main/roms/microsoft/Windows7/docker-compose.yml"
 curl -o /userdata/roms/microsoft/windows7/Windows7.sh https://github.com/garbagescow/Windows/main/roms/microsoft/Windows7/Windows7.sh
 chmod +x /userdata/roms/microsoft/windows7/Windows7.sh
 
-# Verificar se docker-compose está instalado
+# Check if docker-compose is installed
 if ! command -v docker-compose &> /dev/null
 then
-    echo "docker-compose não está instalado. Instale docker-compose e tente novamente."
+    echo "docker-compose is not installed. Please install docker-compose and try again."
     exit 1
 fi
-#Baixar e mover ES para a pasta 
+
+# Download and move ES to the folder
 curl -o /userdata/system/configs/emulationstation/es_systems_microsoft.cfg https://github.com/garbagescow/Windows/main/es_systems_microsoft.cfg
 
-cd /userdata/roms/microsoft/windows7 && docker-compose up  
+cd /userdata/roms/microsoft/windows7 && docker-compose up
 pkill xterm
 
-# Abrir Firefox com o endereço localhost
+# Open Firefox with the localhost address
 /userdata/system/pro/ffnightly/Launcher http://localhost:8006
-
-
-
-
