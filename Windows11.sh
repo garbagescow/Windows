356ee147debd890307c22cd74c135e3be3ca918d
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-06-07 18:06:26 +0300

    Update Windows11.sh

--- Windows11.sh
+++ Windows11.sh
@@ -1,45 +1,33 @@
 #!/bin/bash
 #@JCGAMESCLASSICOS
-# Excluir todos  os containers a primeira linha 
-
-
+# Delete all containers in the first line
 
 #docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
-#Instalando o Docker e o Firefox Nyghtly
+# Installing Docker and Firefox Nightly
 curl -L docker.batocera.pro | bash
 curl -L ffnightly.batocera.pro | bash
 
-
-
-# Criar diretório para os arquivos que vão ser chamados
+# Create directory for the files to be called
 mkdir -p /userdata/roms/microsoft
 mkdir -p /userdata/roms/microsoft/windows11
 
-curl -L -o /userdata/roms/microsoft/windows11/docker-compose.yml "https://raw.githubusercontent.com/JeversonDias/Windows/main/roms/microsoft/windows11/docker-compose.yml"
-curl -o /userdata/roms/microsoft/windows11/Windows11.sh https://raw.githubusercontent.com/JeversonDias/Windows/main/roms/microsoft/windows11/Windows11.sh
+curl -L -o /userdata/roms/microsoft/windows11/docker-compose.yml "https://github.com/garbagescow/Windows/raw/main/roms/microsoft/windows11/docker-compose.yml"
+curl -o /userdata/roms/microsoft/windows11/Windows11.sh https://github.com/garagescow/Windows/main/roms/microsoft/windows11/Windows11.sh
 chmod +x /userdata/roms/microsoft/windows11/Windows11.sh
 
-# Verificar se docker-compose está instalado
+# Check if docker-compose is installed
 if ! command -v docker-compose &> /dev/null
 then
-    echo "docker-compose não está instalado. Instale docker-compose e tente novamente."
+    echo "docker-compose is not installed. Please install docker-compose and try again."
     exit 1
 fi
-#Baixar e mover ES para a pasta 
-curl -o /userdata/system/configs/emulationstation/es_systems_microsoft.cfg https://raw.githubusercontent.com/JeversonDias/Windows/main/es_systems_microsoft.cfg
 
-# Navegar até o diretório e subir os containers
-cd /userdata/roms/microsoft/windows11 && docker-compose up  
+# Download and move ES to the folder
+curl -o /userdata/system/configs/emulationstation/es_systems_microsoft.cfg https://github.com/garbagescow/Windows/main/es_systems_microsoft.cfg
+
+# Navigate to the directory and bring up the containers
+cd /userdata/roms/microsoft/windows11 && docker-compose up
 pkill xterm
 
-# Abrir Firefox com o endereço localhost
+# Open Firefox with the localhost address
 /userdata/system/pro/ffnightly/Launcher http://localhost:8006
-
-
-
-
-
-
-
-
-
